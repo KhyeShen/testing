16c69972fe84419e4bfa264650c2eade2bc19a97
KhyeShen <99790267+KhyeShen@users.noreply.github.com>
2022-10-16 15:54:15 +0800

    Excellent

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+    <meta charset="utf-8"/>
+    <TITLE>Upload Image to MySQL BLOB</TITLE>
+</HEAD>
+<BODY>
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+	echo "Excellent";
 ?>
-Something is wrong with the XAMPP installation :-(
+<div class="card">
+    <div class="card-header"><i class="fa fa-fw fa-download"></i> <strong>Download In Zip</strong></div>
+    <div class="card-body">
+        <hr>
+        <div class="table table-responsive">
+            <form name="zips" method="post"> 
+                
+                <button type="submit" name="createzip" id="createzip" value="createzip" class="btn btn-primary"><i class="fa fa-archive"></i> Download All</button>
+                        
+            </form>
+        </div>
+    </div>
+</div>
+</BODY>
+</HTML>
